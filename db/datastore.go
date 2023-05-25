82af25c4177a9765c9b4a9a17243118beecedf91
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2023-05-25 15:12:30 +0100

    fix : fix bugs on sync dependencies

--- db/datastore.go
+++ db/datastore.go
@@ -1,4 +1,20 @@
 package db
 
-type Datastore interface {
+import "github.com/DecodeWorms/server-contract/models"
+// DataStore interface
+//
+//go:generate mockgen -source=datastore.go -destination=../mocks/datastore_mock.go -package=mocks
+type DataStore interface {
+	PlayerStore
+}
+type PlayerStore interface {
+	CreatePlayer(data models.PersonalInfo) error
+	GetPlayerById(id string) (*models.PersonalInfo, error)
+	GetPlayerByPhoneNumber(phoneNumber string) (*models.PersonalInfo, error)
+	UpdatePlayer(id string, data models.PersonalInfo) error
+	CreatePlayerWithFieldsData(data models.FieldInfo) error
+	GetPlayer(jerseyNumber string) (*models.FieldInfo, error)
+	UpdatePlayerWithFieldsInfo(id string, data models.FieldInfo) (*models.FieldInfo, error)
+	GetPlayerWithFieldsInfoById(id string) (*models.FieldInfo, error)
+	DeletePlayer(id string) error
 }
